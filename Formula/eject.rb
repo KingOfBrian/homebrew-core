923f05c12a83492d1a84339f4f5dde5fd6a76758
Brian King <brian.king@raizlabs.com>
2016-11-29 20:46:36 -0500

    Eject 0.1.27

--- Formula/eject.rb
+++ Formula/eject.rb
@@ -1,8 +1,8 @@
 class Eject < Formula
   desc "Generate swift code from Interface Builder xibs"
   homepage "https://github.com/Raizlabs/Eject"
-  url "https://github.com/Raizlabs/Eject/archive/0.1.23.tar.gz"
-  sha256 "6edd4bd393981f8e1a7b5b7b8f29b5594d17ecf7f55a3e81098a88191c02ae71"
+  url "https://github.com/Raizlabs/Eject/archive/0.1.27.tar.gz"
+  sha256 "b4aa8d281074074632422e9e8583d50024f1b2712566fae7950e73f751f75791"
 
   bottle do
     cellar :any
